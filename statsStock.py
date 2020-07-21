b0239f5f9a8d10c0a66af8298e4686bb566e9bbc
zhangsheng377 <435878393@qq.com>
2020-07-22 01:13:41 +0800

    优化显示范围和下标，显示中文，去掉误差图，新增原始数据图

--- statsStock.py
+++ statsStock.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.use('Agg')
 from pandas.plotting import register_matplotlib_converters
 from statsmodels.tsa.seasonal import STL
@@ -14,13 +15,15 @@ register_matplotlib_converters()
 sns.set_style('darkgrid')
 plt.rc('figure', figsize=(16, 12))
 plt.rc('font', size=13)
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 
 class StatsStock:
     def __init__(self):
         with open("token", 'r') as file:
             token = file.readline()
-        self.__start_time = datetime.date.today() + datetime.timedelta(-365)
+        self.__start_time = datetime.date.today() + datetime.timedelta(-365*0.6)
         self.__x_label_gap = 30
         self.__pro = ts.pro_api(token=token)
         return
@@ -37,38 +40,43 @@ class StatsStock:
 
             data = df_data['open'].tolist()
             data = pd.Series(data,
-                             index=pd.date_range(start=self.__start_time.strftime("%d-%m-%Y"), periods=len(data), freq='D'),
+                             index=pd.date_range(start=self.__start_time.strftime("%d-%m-%Y"), periods=len(data),
+                                                 freq='D'),
                              name='open')
             # print(data)
 
             stl = STL(data)
             res = stl.fit()
 
-            #plt.figure()
+            # plt.figure()
             fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
 
-            ax1.set_title('trend')
-            ax1.plot(df_data['trade_date'], res.trend)
+            ax1.set_title('原始数据')
+            ax1.plot(df_data['trade_date'], data)
             for label in ax1.get_xticklabels():
                 label.set_visible(False)
-            for label in ax1.get_xticklabels()[::self.__x_label_gap]:
+            for label in ax1.get_xticklabels()[:-self.__x_label_gap:self.__x_label_gap]:
                 label.set_visible(True)
+            ax1.get_xticklabels()[-1].set_visible(True)
 
-            ax2.set_title('seasonal')
-            ax2.plot(df_data['trade_date'], res.seasonal)
+            ax2.set_title('趋势')
+            ax2.plot(df_data['trade_date'], res.trend)
             for label in ax2.get_xticklabels():
                 label.set_visible(False)
-            for label in ax2.get_xticklabels()[::self.__x_label_gap]:
+            for label in ax2.get_xticklabels()[:-self.__x_label_gap:self.__x_label_gap]:
                 label.set_visible(True)
+            ax2.get_xticklabels()[-1].set_visible(True)
 
-            ax3.set_title('resid')
-            ax3.scatter(df_data['trade_date'], res.resid)
+            ax3.set_title('周期')
+            ax3.plot(df_data['trade_date'], res.seasonal)
             for label in ax3.get_xticklabels():
                 label.set_visible(False)
-            for label in ax3.get_xticklabels()[::self.__x_label_gap]:
+            for label in ax3.get_xticklabels()[:-self.__x_label_gap:self.__x_label_gap]:
                 label.set_visible(True)
+            ax3.get_xticklabels()[-1].set_visible(True)
 
             # plt.show()
+            fig.suptitle(ts_code)
             fig.savefig('plot.png')
             return True
         except:
@@ -89,3 +97,5 @@ if __name__ == "__main__":
     print('time cost', time.time() - time_start, 's')
 
     print(datetime.date.today())
+
+    print(matplotlib.get_cachedir())
